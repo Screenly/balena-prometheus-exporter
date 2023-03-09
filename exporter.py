9b46b6f979fa1906eefe8ec4e591a33957ecfd61
Viktor Petersson <self@vpetersson.com>
2023-03-09 18:31:25 +0000

    Linting and formatting tweaks.

--- exporter.py
+++ exporter.py
@@ -1,12 +1,15 @@
-import requests
 import os
 import sys
 import time
-from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
-from prometheus_client import start_http_server
 
-BALENA_TOKEN = os.getenv('BALENA_TOKEN', False)
-CRAWL_INTERVAL = os.getenv('CRAWL_INTERVAL', 60)
+import requests
+from prometheus_client import start_http_server
+from prometheus_client.core import (REGISTRY, CounterMetricFamily,
+                                    GaugeMetricFamily)
+
+BALENA_TOKEN = os.getenv("BALENA_TOKEN", False)
+CRAWL_INTERVAL = os.getenv("CRAWL_INTERVAL", 60)
+
 
 def get_balena_fleets():
     """
@@ -17,20 +20,20 @@ def get_balena_fleets():
     fleets = []
     headers = {
         "Authorization": f"Bearer {BALENA_TOKEN}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     response = requests.get(
         "https://api.balena-cloud.com/v6/application?$filter=is_directly_accessible_by__user/any(dau:1 eq 1)",
-        headers=headers
+        headers=headers,
     )
 
     if not response.ok:
         print("Error: {}".format(response.text))
         sys.exit(1)
 
-    for fleet in response.json()['d']:
-        fleets.append((fleet['id']))
+    for fleet in response.json()["d"]:
+        fleets.append((fleet["id"]))
 
     return fleets
 
@@ -38,29 +41,32 @@ def get_balena_fleets():
 def get_fleet_metrics(fleet_id):
     headers = {
         "Authorization": f"Bearer {BALENA_TOKEN}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     response = requests.get(
         f"https://api.balena-cloud.com/v6/application({fleet_id})?$expand=owns__device/$count($filter=is_online eq true)",
-        headers=headers
+        headers=headers,
     )
 
     if not response.ok:
         print("Error: {}".format(response.text))
         sys.exit(1)
 
-    device_online_count = response.json()['d'][0]['owns__device']
-    fleet_name = response.json()['d'][0]['app_name']
+    device_online_count = response.json()["d"][0]["owns__device"]
+    fleet_name = response.json()["d"][0]["app_name"]
 
     return fleet_name, device_online_count
+
 
 class BalenaCollector(object):
     def __init__(self):
         pass
 
     def collect(self):
-        gauge = GaugeMetricFamily("balena_devices_online", "Devices by status", labels=["fleet_name"])
+        gauge = GaugeMetricFamily(
+            "balena_devices_online", "Devices by status", labels=["fleet_name"]
+        )
 
         for fleet_id in get_balena_fleets():
             fleet_name, device_online_count = get_fleet_metrics(str(fleet_id))
@@ -68,6 +74,7 @@ class BalenaCollector(object):
             gauge.add_metric([fleet_name], float(device_online_count))
 
         yield gauge
+
 
 def main():
     if not BALENA_TOKEN:
